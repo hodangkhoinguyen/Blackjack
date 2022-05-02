74e126b9fb7354dc8c3b3a3119cb80f28998c865
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-05-02 12:09:36 -0700

    preview page done.

--- public/videoData.js
+++ public/videoData.js
@@ -1,18 +1,104 @@
-'use strict';
+// for example, these are hardcoded
+var url = 'https://www.tiktok.com/@cheyennebaker1/video/7088856562982423854';
 
 sendGetRequest('/getMostRecent')
 .then(function (data) {
   document.getElementById('c2').innerHTML = "\'" + data.nickname + "\'";
   console.log(data.url);
+  url = data.url;
+  
+  // add the blockquote element that TikTok wants to load the video into
+  addVideo(url,divElmt);
+
+  // on start-up, load the videos
+  loadTheVideos();
 })
 .catch(function (error) {
    console.error('Error:', error);
 });
 
-const continueButton = document.getElementById("continue_button");
+// grab elements we'll use 
+// these are global! 
+let continueButton = document.getElementById("continue");
+let reloadButton = document.getElementById("reload");
+let divElmt = document.getElementById("tiktokDiv");
+
 continueButton.addEventListener("click", continueFunction);
+reloadButton.addEventListener("click", reloadVideo);
 
+// Add the blockquote element that tiktok will load the video into
+async function addVideo(tiktokurl,divElmt) {
 
+  let videoNumber = tiktokurl.split("video/")[1];
+
+  let block = document.createElement('blockquote');
+  block.className = "tiktok-embed";
+  block.cite = tiktokurl;
+  // have to be formal for attribute with dashes
+  block.setAttribute("data-video-id",videoNumber);
+  block.style = "width: 325px; height: 563px;"
+
+  let section = document.createElement('section');
+  block.appendChild(section);
+  
+  divElmt.appendChild(block);
+}
+
+// Ye olde JSONP trick; to run the script, attach it to the body
+function loadTheVideos() {
+  body = document.body;
+  script = newTikTokScript();
+  body.appendChild(script);
+}
+
+// makes a script node which loads the TikTok embed script
+function newTikTokScript() {
+  let script = document.createElement("script");
+  script.src = "https://www.tiktok.com/embed.js"
+  script.id = "tiktokScript"
+  return script;
+}
+
+// the reload button; takes out the blockquote and the scripts, and puts it all back in again.
+// the browser thinks it's a new video and reloads it
+function reloadVideo () {
+  
+  // get the two blockquotes
+  let blockquotes 
+ = document.getElementsByClassName("tiktok-embed");
+
+  // and remove the indicated one
+    block = blockquotes[0];
+    console.log("block",block);
+    let parent = block.parentNode;
+    parent.removeChild(block);
+
+  // remove both the script we put in and the
+  // one tiktok adds in
+  let script1 = document.getElementById("tiktokScript");
+  let script2 = script.nextElementSibling;
+
+  let body = document.body; 
+  body.removeChild(script1);
+  body.removeChild(script2);
+
+  addVideo(url,divElmt);
+  loadTheVideos();
+}
+
+function continueFunction() {
+  let info = "acknowledgement done!";
+  
+  sendPostRequest('/acknowledgement', info)
+  .then(function (data) {
+    console.log("got back the following string");
+    console.log(data); 
+    window.location = "/tiktokpets.html";
+  })
+  .catch(function (error) {
+     console.error('Error:', error);
+  });
+}
 
 async function sendGetRequest(url) {
   console.log("about to send GET request");
@@ -41,17 +127,3 @@ async function sendPostRequest(url, data) {
       throw Error(response.status);
     }
   }
-
-function continueFunction() {
-  let info = "acknowledgement done!";
-  
-  sendPostRequest('/acknowledgement', info)
-  .then(function (data) {
-    console.log("got back the following string");
-    console.log(data); 
-    window.location = "/tiktokpets.html";
-  })
-  .catch(function (error) {
-     console.error('Error:', error);
-  });
-}
