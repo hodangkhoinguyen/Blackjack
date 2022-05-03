25ed7dff1ccca267a512c0d35fe455428031dcbe
Michaelangelo Plete <103979929+plete-michaelangelo@users.noreply.github.com>
2022-05-03 21:11:03 +0000

    myVideos.js wip

--- public/myVideos.js
+++ public/myVideos.js
@@ -1,10 +1,17 @@
 'use strict'
 
 for (let i=0; i<8; i++) { 
-parent = document.getElementById("video-holder");
-div = document.createElement("div"); div.classList.add("videoLine"); 
-p = document.createElement("p"); 
+var parent = document.getElementById("video-holder");
+var div = document.createElement("div"); div.classList.add("videoLine"); 
+var p = document.createElement("p"); 
+var x = document.createElement("button");
   p.classList.add("videoName"); 
+  x.classList.add("x");
+  let bt = x.textContent;
+  bt = "X";
   div.appendChild(p); 
+  div.appendChild(x);
   parent.appendChild(div); 
 } 
+
+let names = document.getElementsByClassname("videoName"); let buttons = document.getElementsByClassname("x");
